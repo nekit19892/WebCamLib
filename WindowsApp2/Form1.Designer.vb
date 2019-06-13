51bd537ad49fc2eadb91845c31b43bee410caba5
Сергеев Никита <letter20016@gmail.com>
2019-06-13 20:44:23 +0300

    Добавьте файлы проекта.

---
+++ WindowsApp2/Form1.Designer.vb
@@ -0,0 +1,101 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.dynamicDotNetTwain1 = New Dynamsoft.DotNet.TWAIN.DynamicDotNetTwain()
+        Me.Button2 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 45)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(280, 300)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(25, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(119, 27)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Старт"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'dynamicDotNetTwain1
+        '
+        Me.dynamicDotNetTwain1.AnnotationFillColor = System.Drawing.Color.White
+        Me.dynamicDotNetTwain1.AnnotationPen = Nothing
+        Me.dynamicDotNetTwain1.AnnotationTextColor = System.Drawing.Color.Black
+        Me.dynamicDotNetTwain1.AnnotationTextFont = Nothing
+        Me.dynamicDotNetTwain1.BorderStyle = Dynamsoft.DotNet.TWAIN.Enums.DWTWndBorderStyle.Single3D
+        Me.dynamicDotNetTwain1.IfShowCancelDialogWhenImageTransfer = False
+        Me.dynamicDotNetTwain1.IfShowPrintUI = False
+        Me.dynamicDotNetTwain1.IfThrowException = False
+        Me.dynamicDotNetTwain1.Location = New System.Drawing.Point(12, 45)
+        Me.dynamicDotNetTwain1.LogLevel = CType(1, Short)
+        Me.dynamicDotNetTwain1.Name = "dynamicDotNetTwain1"
+        Me.dynamicDotNetTwain1.PDFMarginBottom = CType(0UI, UInteger)
+        Me.dynamicDotNetTwain1.PDFMarginLeft = CType(0UI, UInteger)
+        Me.dynamicDotNetTwain1.PDFMarginRight = CType(0UI, UInteger)
+        Me.dynamicDotNetTwain1.PDFMarginTop = CType(0UI, UInteger)
+        Me.dynamicDotNetTwain1.PDFXConformance = CType(0UI, UInteger)
+        Me.dynamicDotNetTwain1.ProductFamily = "Dynamic .NET TWAIN Trial (.NET Fr"
+        Me.dynamicDotNetTwain1.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.dynamicDotNetTwain1.Size = New System.Drawing.Size(280, 300)
+        Me.dynamicDotNetTwain1.TabIndex = 13
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(176, 12)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(107, 26)
+        Me.Button2.TabIndex = 14
+        Me.Button2.Text = "Стоп"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(308, 357)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.dynamicDotNetTwain1)
+        Me.Name = "Form1"
+        Me.Text = "WebCamView"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button1 As Button
+    Private WithEvents dynamicDotNetTwain1 As Dynamsoft.DotNet.TWAIN.DynamicDotNetTwain
+    Friend WithEvents Button2 As Button
+End Class
---
+++ WindowsApp2/Form1.vb
@@ -0,0 +1,24 @@
+﻿Imports Dynamsoft.DotNet.TWAIN.Enums
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            dynamicDotNetTwain1.SupportedDeviceType = EnumSupportedDeviceType.SDT_WEBCAM
+            dynamicDotNetTwain1.SelectSource()
+            dynamicDotNetTwain1.IfShowUI = True
+            dynamicDotNetTwain1.SetVideoContainer(Me.PictureBox1)
+            dynamicDotNetTwain1.OpenSource()
+        Catch exp As Exception
+            MessageBox.Show(exp.Message)
+        End Try
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        dynamicDotNetTwain1.IfShowUI = False
+        dynamicDotNetTwain1.CloseSource()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
